966ec9fb524589dfc954fb531344171016e250b1
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-11-28 14:06:34 +0400

    feat: Listed Products

---
+++ Stock-MVC/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Stock_MVC.Models.Entity;
+namespace Stock_MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        StockProjectMVCEntities db = new StockProjectMVCEntities();
+        public ActionResult Index()
+        {
+            var deyerler = db.TBL_MEHSULLAR.ToList();
+            return View(deyerler);
+        }
+    }
+}
