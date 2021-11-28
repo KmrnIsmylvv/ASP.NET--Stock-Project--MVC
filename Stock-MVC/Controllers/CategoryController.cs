8344e987eb14fcbdcdc6b51b9db720f932ef6a45
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-11-28 12:09:38 +0400

    feat: added Category List

--- Stock-MVC/Controllers/CategoryController.cs
+++ Stock-MVC/Controllers/CategoryController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Stock_MVC.Models.Entity;
 namespace Stock_MVC.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+
+        StockProjectMVCEntities db = new StockProjectMVCEntities();
         public ActionResult Index()
         {
-            return View();
+            var deyerler = db.TBL_KATEQORIYALAR.ToList();
+            return View(deyerler);
         }
     }
 }
