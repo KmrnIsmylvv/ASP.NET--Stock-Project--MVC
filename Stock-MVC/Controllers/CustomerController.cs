5816836e49f67718e2aa0e1f42eecaa8ef39d0ae
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-18 13:27:50 +0400

    feat: create Update Customer page

--- Stock-MVC/Controllers/CustomerController.cs
+++ Stock-MVC/Controllers/CustomerController.cs
@@ -38,5 +38,19 @@ namespace Stock_MVC.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult MusteriGetir(int id)
+        {
+            var mus = db.TBL_MUSTERILER.Find(id);
+            return View("MusteriGetir", mus);
+        }
+        public ActionResult Guncelle(TBL_MUSTERILER p1)
+        {
+            var musteri = db.TBL_MUSTERILER.Find(p1.MUSTERIID);
+            musteri.MUSTERIAD = p1.MUSTERIAD;
+            musteri.MUSTERISOYAD = p1.MUSTERISOYAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
