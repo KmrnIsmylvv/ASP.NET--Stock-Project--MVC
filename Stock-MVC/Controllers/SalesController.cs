83afb09cd5293a4ddae9c34978175c779bb5cd37
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-18 13:32:57 +0400

    feat: doing sale

--- Stock-MVC/Controllers/SalesController.cs
+++ Stock-MVC/Controllers/SalesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Stock_MVC.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,26 @@ namespace Stock_MVC.Controllers
     public class SalesController : Controller
     {
         // GET: Sales
+        StockProjectMVCEntities db = new StockProjectMVCEntities();
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniSatis(TBL_SATISLAR p)
+        {
+            db.TBL_SATISLAR.Add(p);
+            db.SaveChanges();
+            return View("Index");
+        }
+
+
     }
 }
